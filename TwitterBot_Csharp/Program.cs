96bff8875943323a9e2118addf292a4e202c256d
Kirk Bozeman <kirkbozeman@gmail.com>
2016-11-24 19:03:55 -0600

    Poem pull working

--- TwitterBot_Csharp/Program.cs
+++ TwitterBot_Csharp/Program.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using TweetSharp;
 using TwitterBot_Csharp.Classes;
+using HtmlAgilityPack;
+using CsQuery;
+using System.Net;
 
 namespace TwitterBot_Csharp
 {
@@ -13,38 +16,22 @@ namespace TwitterBot_Csharp
         static void Main(string[] args)
         {
 
-            /* TweetSharp is no longer being updated but is necessary. TweetMoaSharp is the current
-            updated package. Use Update-Package TweetMoaSharp to update it. Use Update-Package -reinstall TweetMoaSharp
-            to reinstall the entire package. It must be installed ON TOP of existing final TweetSharp package.
-             */
+            string url = "https://www.poetryfoundation.org/poems-and-poets/poems/detail/90621";
 
-            string _consumerKey = "licUTuwmj4J16pMy30UFQZofI";
-            string _consumerSecret = "LuLn34qrT7iczsga7RvtJnHf1jncDH4oJU70Btzj9LTz8rjA5E";
-            string _accessToken = "801871349434187776-mcvC35KftjhlR7doV6l6UQzrAWIFbdz";
-            string _accessTokenSecret = "AWClOy005tBXiyxnFUlLVAuDhu269YI5vyh5HSW1mUA2D";
+            WebClient client = new WebClient();
+            string htmlString = client.DownloadString(url);
 
-            var service = new TwitterService(_consumerKey, _consumerSecret);
-            service.AuthenticateWith(_accessToken, _accessTokenSecret);
+            var doc = new HtmlDocument();
+            doc.LoadHtml(htmlString);
 
+            HtmlNode rateNode = doc.DocumentNode.SelectSingleNode("//div[@class='poem']");
+            Console.WriteLine(rateNode.InnerText);
 
-//            service.SendTweet(new SendTweetOptions {Status = "Hello world. #helloworld"});
-
-            var tweets = service.Search(new SearchOptions { Q = "trump", Count = 1, Resulttype = TwitterSearchResultType.Popular, IncludeEntities = false});
-
-//            var tweets = service.ListTweetsOnHomeTimeline(new ListTweetsOnHomeTimelineOptions());
-            foreach (var tweet in tweets.Statuses)
-            {
-                Console.WriteLine("{0}; {1}; {2}", tweet.User.ScreenName, tweet.Text, tweet.Id.ToString());
-                service.SendTweet(new SendTweetOptions { InReplyToStatusId = tweet.Id, Status = "@" + tweet.User.ScreenName + " it's not your fault"});
-                //                service.Retweet(new RetweetOptions {Id = tweet.Id }); // this is how to retweet
-            }
-
-
-//            service.SendTweet
-
+//            HtmlNode specificNode = doc.GetElementbyId("poem");
+            //            HtmlNodeCollection nodesMatchingXPath = doc.DocumentNode.SelectNodes("x/path/nodes");
+//            Console.WriteLine(specificNode.InnerText);
+            //            TwitterConnect.PostToBot("this post");         
             Console.ReadKey();
-             
-
         }
     }
 }
